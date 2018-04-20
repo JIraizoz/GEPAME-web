1b5350d240800ba9dfa21473350bc5818f9fb790
JIraizoz <jesusiraizoz@gmail.com>
2018-04-20 18:03:22 +0200

    Creación servidor y protocolo +LOGIN +BYE

--- TFG-SAHANA/GEPAME-Core/Program.cs
+++ TFG-SAHANA/GEPAME-Core/Program.cs
@@ -1,6 +1,7 @@
 ﻿using GEPAMECore.AD;
 using System;
 using GEPAMECore.LD;
+using GEPAMECore.LN;
 
 namespace GEPAME_Core
 {
@@ -12,7 +13,8 @@ namespace GEPAME_Core
             TipoVehiculo tipoVehiculo = new TipoVehiculo("AM0001", "");
             Vehiculo v = new Vehiculo("2","DEF0123456789","1234AAA","2018",false,true,tipoVehiculo);
 
-            new AD_Vehiculo(new AD_GestorSqlServer("Data Source=localhost;Initial Catalog=GEPAME;Integrated Security=True").Connection).deleteVehiculo("1234AAA");
+            //new AD_Posicion(new AD_GestorSqlServer("Data Source=localhost;Initial Catalog=GEPAME;Integrated Security=True").Connection).GetUltimaPosicion("6969JDT");
+            LN_Server.Server();
 
             Console.ReadKey();
         }
