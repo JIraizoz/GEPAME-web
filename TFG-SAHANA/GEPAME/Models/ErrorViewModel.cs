9e1296aa495a034ee8fa128c7e08ccfd00fa53b2
JIraizoz <jesusiraizoz@gmail.com>
2018-04-21 10:17:32 +0200

    Aplicacion ASP.Net Core

---
+++ TFG-SAHANA/GEPAME/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GEPAME.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
