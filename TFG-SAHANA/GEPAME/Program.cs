7b1f19abf75570da9cc3617eb8a933be53a3655e
JIraizoz <jesusiraizoz@gmail.com>
2018-05-02 08:20:59 +0200

    XSD

--- TFG-SAHANA/GEPAME/Program.cs
+++ TFG-SAHANA/GEPAME/Program.cs
@@ -15,11 +15,23 @@ namespace GEPAME
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
+                .UseKestrel()
+                .UseUrls("http://127.0.0.1:5000/")
+                .UseWebRoot(Directory.GetCurrentDirectory() + "/wwwroot")
+                .ConfigureAppConfiguration((context, configBuilder) =>
+                {
+                    configBuilder
+                        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true)
+                        .AddEnvironmentVariables();
+                })
                 .Build();
     }
 }
